d2e7716e218657a186abcf92bab4aed93826bd71
kahfei <bak.kahfei@gmail.com>
2012-10-06 08:17:22 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clbw93::Application.config.secret_token = '3bbf0ac56edd905bcf30d874aef3ea8520cdf05e9580c5399ca99e11ac5a40af81b53008a682ad1332959eca6126efe9078af2b57eeea673a9344005039e8bc8'
